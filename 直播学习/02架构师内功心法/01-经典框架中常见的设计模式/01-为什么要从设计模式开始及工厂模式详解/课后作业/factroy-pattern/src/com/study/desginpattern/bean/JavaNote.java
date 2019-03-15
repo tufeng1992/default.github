006369e39a21877b190b4ecfe9fa6fc4af83b5b0
to_be_continued <tufeng1992@163.com>
2019-03-16 00:05:34 +0800

    工厂模式作业

---
+++ 直播学习/02架构师内功心法/01-经典框架中常见的设计模式/01-为什么要从设计模式开始及工厂模式详解/课后作业/factroy-pattern/src/com/study/desginpattern/bean/JavaNote.java
@@ -0,0 +1,8 @@
+package com.study.desginpattern.bean;
+
+public class JavaNote implements Note {
+    @Override
+    public String write() {
+        return "java笔记";
+    }
+}
---
+++ 直播学习/02架构师内功心法/01-经典框架中常见的设计模式/01-为什么要从设计模式开始及工厂模式详解/课后作业/factroy-pattern/src/com/study/desginpattern/bean/PythonNote.java
@@ -0,0 +1,8 @@
+package com.study.desginpattern.bean;
+
+public class PythonNote implements Note {
+    @Override
+    public String write() {
+        return "Python的笔记";
+    }
+}
